39d39f537ae4776f83f77c6bb7eea746800559a6
rosangel-birt <rcampusano@birt.eus>
2025-11-28 14:54:47 +0100

    Arreglar filtro de nombre y aplicar formateo en los ficheros

--- js/data/personajes.data.js
+++ js/data/personajes.data.js
@@ -1,7 +1,8 @@
 // Fichero que se encarga de recuperar los personajes
 export function fetchConFiltrosDePersonajes(filtros) {
-    let url = "https://api.potterdb.com/v1/characters?filter[house_not_null]=true";
-    filtros.forEach(filtro => {
+    let url =
+        "https://api.potterdb.com/v1/characters?filter[house_not_null]=true";
+    filtros.forEach((filtro) => {
         url += "&filter[" + filtro.tipo + "]=" + filtro.valor;
     });
     const res = fetch(url);
