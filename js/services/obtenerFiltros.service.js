fdb264c0f4e364aee0b9629af7aeb42feb20af1b
rosangel-birt <rcampusano@birt.eus>
2025-11-28 14:48:13 +0100

    Agregar filtros de vivos y muertos

--- js/services/obtenerFiltros.service.js
+++ js/services/obtenerFiltros.service.js
@@ -1,12 +1,31 @@
 export function extraerFiltros() {
+    const filtros = [];
+
     const filtroNombreElemento = document.getElementById("filtroNombre");
-    const filtroNombre = {
-        tipo: "name_cont",
-        valor: filtroNombreElemento.value,
-    };
+    if (filtroNombreElemento.value != "") {
+        const filtroNombre = {
+            tipo: "name_cont",
+            valor: filtroNombreElemento.value,
+        };
+        filtros.push(filtroNombre);
+    }
 
     const filtroCasasElemento = document.getElementById("filtroCasa");
-    const filtroCasa = { tipo: "house_cont", valor: filtroCasasElemento.value };
+    if (filtroCasasElemento.value != "") {
+        const filtroCasa = {
+            tipo: "house_cont",
+            valor: filtroCasasElemento.value,
+        };
+        filtros.push(filtroCasa);
+    }
 
-    return [filtroNombre, filtroCasa];
+    const filtroEstadoElemento = document.getElementById("filtroVivoOMuerto");
+    if (filtroEstadoElemento.value !== "") {
+        const filtroEstado = {
+            tipo: "died_not_null",
+            valor: filtroEstadoElemento.value === "muerto" ? "true" : "false",
+        };
+        filtros.push(filtroEstado);
+    }
+    return filtros;
 }
